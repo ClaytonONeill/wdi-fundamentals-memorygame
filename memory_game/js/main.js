afa75aca75cbff3fa574befb953d1b5473e41d1a
Clay <clayton.d.oneill@gmail.com>
2020-01-15 14:04:01 -0500

    Added logic to the game

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+
+let cardOne = "King";
+let cardTwo = "Queen";
+let cardThree = "Queen";
+let cardFour = "King";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
