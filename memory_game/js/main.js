bf9efabd10cb6f3be79200e2e6e77536bb642928
Clay <clayton.d.oneill@gmail.com>
2020-01-15 20:58:54 -0500

    added more logic and arrays

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
-console.log("Up and Running!");
 
-let cardOne = "King";
-let cardTwo = "Queen";
-let cardThree = "Queen";
-let cardFour = "King";
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+console.log("User flipped" + 'queen');
+console.log("User flipped" + 'king');
+
+const cards = ['queen', "queen", "king", "king"];
+const cardsInPlay = [];
+
+cardsInPlay.length;
+
+const cardOne = cards[0];
+
+cardsInPlay.push(cards[0]);
+
+const cardTwo = cards[2];
+
+cardsInPlay.push(cards[2]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert ("You found a match!");
+	else {
+		alert("Sorry try again");
+	}
+}
